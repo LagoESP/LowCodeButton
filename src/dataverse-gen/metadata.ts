cebffcf01920523c57fb73310860e71c1387229d
Jose Lago <jose.lago@be-terna.com>
2025-03-19 09:59:52 +0100

    delete account earlybound

--- src/dataverse-gen/metadata.ts
+++ src/dataverse-gen/metadata.ts
@@ -1,11 +1,9 @@
 /* eslint-disable*/
-import { accountMetadata } from "./entities/Account";
 import { esp_buttonadvancedsettingMetadata } from "./entities/esp_ButtonAdvancedSetting";
 import { esp_buttonsettingsMetadata } from "./entities/esp_ButtonSettings";
 import { esp_languageMetadata } from "./entities/esp_Language";
 
 export const Entities = {
-  Account: "account",
   esp_ButtonAdvancedSetting: "esp_buttonadvancedsetting",
   esp_ButtonSettings: "esp_buttonsettings",
   esp_Language: "esp_language",
@@ -15,7 +13,6 @@ export const Entities = {
 // Usage: setMetadataCache(metadataCache);
 export const metadataCache = {
   entities: {
-    account: accountMetadata,
     esp_buttonadvancedsetting: esp_buttonadvancedsettingMetadata,
     esp_buttonsettings: esp_buttonsettingsMetadata,
     esp_language: esp_languageMetadata,
