90481fd18c9e19124c7d0e499318eff1adae7014
Lago <jose_manuel_99@hotmail.com>
2025-03-28 12:39:53 +0100

    Update Base Helper

--- src/Helpers/BaseHelper.ts
+++ src/Helpers/BaseHelper.ts
@@ -206,58 +206,57 @@ export class BaseHelper {
     }
   }
 
-  /**
-   * Retrieves the advanced button setting record using the main button setting ID and language code.
-   *
-   * @param mainButtonSettingId - The ID of the main button setting.
-   * @param languageId - The language code.
-   * @returns A Promise that resolves to the esp_ButtonAdvancedSetting or null.
-   * @throws An error if the fetch fails.
-   */
   public async getAllButtonAdvancedSettingExceptTheGivenLCID(
     mainButtonSettingId: string,
     languageId: string,
   ): Promise<esp_ButtonAdvancedSetting[]> {
+    const fetchXml = `
+      <fetch>
+        <entity name="esp_buttonadvancedsetting">
+          <attribute name="esp_asyncformnotification" />
+          <attribute name="esp_asyncformnotificationtext" />
+          <attribute name="esp_buttonadvancedsettingid" />
+          <attribute name="esp_confirmationdialogcancellabel" />
+          <attribute name="esp_confirmationdialogconfirmlabel" />
+          <attribute name="esp_confirmationdialogsubtitle" />
+          <attribute name="esp_confirmationdialogtext" />
+          <attribute name="esp_confirmationdialogtitle" />
+          <attribute name="esp_executionmode" />
+          <attribute name="esp_mainbuttonsetting" />
+          <attribute name="esp_modificationneededflag" />
+          <attribute name="esp_settingid" />
+          <attribute name="esp_settinglanguage" />
+          <attribute name="esp_showconfirmationdialog" />
+          <attribute name="esp_syncconfirmationboxconfirmlabel" />
+          <attribute name="esp_syncconfirmationboxredirectcancellabel" />
+          <attribute name="esp_syncconfirmationboxredirectconfirmlabel" />
+          <attribute name="esp_syncconfirmationboxredirectmode" />
+          <attribute name="esp_syncconfirmationboxredirectsubtitle" />
+          <attribute name="esp_syncconfirmationboxredirecttext" />
+          <attribute name="esp_syncconfirmationboxredirecttitle" />
+          <attribute name="esp_syncconfirmationboxtext" />
+          <attribute name="esp_syncconfirmationboxtitle" />
+          <attribute name="esp_syncconfirmationboxtype" />
+          <attribute name="esp_syncformnotification" />
+          <attribute name="esp_syncformnotificationtext" />
+          <attribute name="esp_syncrefreshform" />
+          <attribute name="esp_syncspinner" />
+          <attribute name="esp_syncspinnertext" />
+          <attribute name="esp_syncsuccessformnotification" />
+          <attribute name="esp_syncsuccessformnotificationtext" />
+          <filter>
+            <condition attribute="esp_mainbuttonsetting" operator="eq" value="${mainButtonSettingId}" />
+            <condition attribute="esp_settinglanguage" operator="ne" value="${languageId}" />
+          </filter>
+        </entity>
+      </fetch>
+    `;
+
+    const query = `?fetchXml=${encodeURIComponent(fetchXml)}`;
+    const url = `${Xrm.Utility.getGlobalContext().getClientUrl()}/api/data/v9.1/esp_buttonadvancedsettings${query}`;
+
     try {
-      const filter = `_esp_mainbuttonsetting_value eq '${mainButtonSettingId}' and esp_settinglanguage/esp_settinglanguageid ne ${languageId}`;
-      const select = [
-        "esp_asyncformnotification",
-        "esp_asyncformnotificationtext",
-        "esp_buttonadvancedsettingid",
-        "esp_confirmationdialogcancellabel",
-        "esp_confirmationdialogconfirmlabel",
-        "esp_confirmationdialogsubtitle",
-        "esp_confirmationdialogtext",
-        "esp_confirmationdialogtitle",
-        "esp_executionmode",
-        "esp_mainbuttonsetting",
-        "esp_modificationneededflag",
-        "esp_settingid",
-        "esp_settinglanguage",
-        "esp_showconfirmationdialog",
-        "esp_syncconfirmationboxconfirmlabel",
-        "esp_syncconfirmationboxredirectcancellabel",
-        "esp_syncconfirmationboxredirectconfirmlabel",
-        "esp_syncconfirmationboxredirectmode",
-        "esp_syncconfirmationboxredirectsubtitle",
-        "esp_syncconfirmationboxredirecttext",
-        "esp_syncconfirmationboxredirecttitle",
-        "esp_syncconfirmationboxtext",
-        "esp_syncconfirmationboxtitle",
-        "esp_syncconfirmationboxtype",
-        "esp_syncformnotification",
-        "esp_syncformnotificationtext",
-        "esp_syncrefreshform",
-        "esp_syncspinner",
-        "esp_syncspinnertext",
-        "esp_syncsuccessformnotification",
-        "esp_syncsuccessformnotificationtext",
-      ];
-
-      const url = `${Xrm.Utility.getGlobalContext().getClientUrl()}/api/data/v9.1/esp_buttonadvancedsettings?$filter=${encodeURIComponent(
-        filter,
-      )}&$select=${select.join(",")}&$expand=esp_settinglanguage($select=esp_lcid)`;
-
+      debugger;
       const response = await this.makeRequest("GET", url);
       const data = await response.json();
       return data.value as esp_ButtonAdvancedSetting[];
@@ -269,7 +268,6 @@ export class BaseHelper {
       return [];
     }
   }
-
   /**
    * Initializes settings by retrieving the button setting (using its name) and then the advanced button setting.
    *
